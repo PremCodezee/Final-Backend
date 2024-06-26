f81d031d61049e56be953bd84ed6c40ccd77444a
Prem Codezee <prem.codezee.gmail.com>
2024-06-26 12:32:04 +0530

    Api response and error handling

---
+++ src/utilities/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+  async (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// another method
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(error || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
