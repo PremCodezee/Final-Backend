717383e146fc2afd5c3eb98eafba4bd02c823606
Prem Codezee <prem.codezee.gmail.com>
2024-06-26 16:10:43 +0530

    upload in backend

---
+++ src/utilities/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload file
+    const uploadResponse = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploded successfully
+    console.log("file is uploaded in cloudinary", uploadResponse.url);
+    return uploadResponse;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporay aved temporary file as the upload operation failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
